d5653537eddc10f15be55c53780d92fee4d82761
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-05-14 12:51:48 +0200

    feat(command): add command static class for handle plazza command

---
+++ src/Reception/Commands.cpp
@@ -0,0 +1,15 @@
+//
+// Created by pavel on 13/05/2026.
+//
+
+#include "Commands.hpp"
+
+#include <iostream>
+
+void Commands::status(Reception& reception, [[maybe_unused]] const std::string& order = "") {
+    std::cout << "STATUS" << std::endl;
+}
+
+void Commands::order(Reception& reception, std::string order) {
+    std::cout << "ORDER" << std::endl;
+}
