6d70bd04959f7973fb95288e58ebe5f0e2efc5e0
Akinator31 <pavel.de-wavrechin@epitech.eu>
2026-05-14 12:48:46 +0200

    refactor(errors): add errors to plazza exception class

--- src/Errors/PlazzaErrors.hpp
+++ src/Errors/PlazzaErrors.hpp
@@ -9,11 +9,23 @@
 
 enum PlazzaErrorTypes {
     IncorrectArgs,
+    ForkError,
+    PollError,
+    SocketCreationError,
+    BindError,
+    ListenError,
+    AcceptError,
 };
 
 class PlazzaException : public std::exception {
     std::map<PlazzaErrorTypes, std::string> _error_map {
-        {IncorrectArgs, "Incorrect arguments!"}
+        {IncorrectArgs, "Incorrect arguments!"},
+        {ForkError, "Fail to fork!"},
+        {PollError, "An error occurred while polling reception socket!"},
+        {SocketCreationError, "An error occurred while creating a socket!"},
+        {BindError, "An error occurred while binding the reception socket!"},
+        {ListenError, "An error occurred while listening the reception socket!"},
+        {AcceptError, "An error occurred while accepting connection on the reception socket!"}
     };
 
     std::string _error;
