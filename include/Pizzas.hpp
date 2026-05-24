50c52bb8cb3be9a423f0108cad82bb03a8ec55a1
Nogo4 <noah.gomes@epitech.eu>
2026-05-24 23:28:31 +0200

    feat(pizza): define Ingredient and PizzaRecipe structs, and update pizzaRecipes

--- include/Pizzas.hpp
+++ include/Pizzas.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 #include <cstdint>
+#include <vector>
+#include <map>
 
 enum class MessageType : uint8_t {
     Order,
@@ -31,4 +33,78 @@ struct Message {
     PizzaType pizzaType;
     PizzaSize pizzaSize;
     uint8_t pizzaNumber;
+};
+
+enum class IngredientType {
+    Dough,
+    Tomato,
+    Gruyere,
+    Ham,
+    Mushrooms,
+    Steak,
+    Eggplant,
+    GoatCheese,
+    ChiefLove
+};
+
+struct Ingredient {
+    IngredientType type;
+    int quantity;
+};
+
+struct PizzaOrder {
+    PizzaSize size;
+    PizzaType type;
+};
+
+struct PizzaRecipe {
+    PizzaType type;
+    std::vector<Ingredient> ingredients;
+    uint32_t cookingTime;
+};
+
+using Stock = std::map<IngredientType, int>;
+
+const std::vector<PizzaRecipe> pizzaRecipes = {
+    {
+        PizzaType::Margarita,
+        {
+            {IngredientType::Dough, 1},
+            {IngredientType::Tomato, 1},
+            {IngredientType::Gruyere, 1}
+        },
+        1
+    },
+    {
+        PizzaType::Regina,
+        {
+            {IngredientType::Dough, 1},
+            {IngredientType::Tomato, 1},
+            {IngredientType::Gruyere, 1},
+            {IngredientType::Ham, 1},
+            {IngredientType::Mushrooms, 1}
+        },
+        2
+    },
+    {
+        PizzaType::Americana,
+        {
+            {IngredientType::Dough, 1},
+            {IngredientType::Tomato, 1},
+            {IngredientType::Gruyere, 1},
+            {IngredientType::Steak, 1}
+        },
+        2
+    },
+    {
+        PizzaType::Fantasia,
+        {
+            {IngredientType::Dough, 1},
+            {IngredientType::Tomato, 1},
+            {IngredientType::Eggplant, 1},
+            {IngredientType::GoatCheese, 1},
+            {IngredientType::ChiefLove, 1}
+        },
+        4
+    }
 };
