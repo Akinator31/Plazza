17ebb280b710ab1c831c4a2b67fc9b2c1df6ce49
Nogo4 <noah.gomes@epitech.eu>
2026-05-24 22:36:48 +0200

    feat(semaphore): add Semaphore class with wait and post methods

---
+++ src/Thread/Semaphore.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <semaphore.h>
+
+class Semaphore {
+    sem_t _semaphore;
+
+public:
+    explicit Semaphore(unsigned int value = 0);
+    ~Semaphore();
+
+    Semaphore(const Semaphore &) = delete;
+    Semaphore &operator=(const Semaphore &) = delete;
+
+    void wait();
+    void post();
+};
